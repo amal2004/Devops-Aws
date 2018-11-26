1b9d443268bbaf6d66002dce0cb6677c062a5d5f
Amal Wee <amal2004@gmail.com>
2018-11-25 22:49:08 -0500

    Added Helloworldcontroller and index.html

---
+++ src/main/java/com/amal/devopsaws/controllers/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.amal.devopsaws.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String sayHello(){
+        return "index";
+    }
+}
