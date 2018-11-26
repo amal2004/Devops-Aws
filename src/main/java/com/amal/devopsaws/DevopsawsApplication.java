e2ea7374138e951b95723aa818819fc9909de73b
Amal Wee <amal2004@gmail.com>
2018-11-25 22:36:22 -0500

    Init project commit

---
+++ src/main/java/com/amal/devopsaws/DevopsawsApplication.java
@@ -0,0 +1,12 @@
+package com.amal.devopsaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsawsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsawsApplication.class, args);
+    }
+}
