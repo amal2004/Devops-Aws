488ec14e23b0fca4edc60392b180b0d3da1a5d96
Amal Wee <amal2004@gmail.com>
2018-11-28 14:02:45 -0500

    Added bootstrap and jquery webjars

--- src/test/java/com/amal/devopsaws/DevopsawsApplicationTest.java
+++ src/test/java/com/amal/devopsaws/DevopsawsApplicationTest.java
@@ -1,5 +1,6 @@
 package com.amal.devopsaws;
 
+import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,10 +8,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DevopsawsApplicationTests {
+public class DevopsawsApplicationTest {
 
     @Test
     public void contextLoads() {
     }
-
 }
