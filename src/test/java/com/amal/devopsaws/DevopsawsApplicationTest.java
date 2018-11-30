5ca434940876fbd9f1c86773914ab3e7f22cb33f
Amal Wee <amal2004@gmail.com>
2018-11-29 23:21:15 -0500

    Added i18 capability

--- src/test/java/com/amal/devopsaws/DevopsawsApplicationTest.java
+++ src/test/java/com/amal/devopsaws/DevopsawsApplicationTest.java
@@ -1,8 +1,12 @@
 package com.amal.devopsaws;
 
 import static org.junit.Assert.*;
+
+import com.amal.devopsaws.web.i18n.I18NService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -10,7 +14,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DevopsawsApplicationTest {
 
+    @Autowired
+    private I18NService i18NService;
+
     @Test
-    public void contextLoads() {
+    public void testMessageByLocaleService() throws Exception {
+        String expectedResult = "Bootstrap starter template1";
+        String messageId = "index.main.callout";
+        String actual = i18NService.getMessage(messageId);
+        Assert.assertEquals("The actual and expected Strings don't match", expectedResult, actual);
     }
 }
